dcc3c0640f035c1ad968b92e5627977c3a858534
ingfosbreak <ingfosbreak@outlook.com>
2023-06-05 23:18:19 +0700

    Add: script and foodlists

---
+++ script.js
@@ -0,0 +1,13 @@
+function chbg1(color) {
+    document.body.style.background = color;
+    document.getElementById('speech1').style.color = 'white'
+    document.getElementById('speech2').style.color = 'white'
+    
+}  
+
+function chbg2(color) {
+    document.body.style.background = color;
+    document.getElementById('speech1').style.color = 'black'
+    document.getElementById('speech2').style.color = 'black'
+    
+}  
