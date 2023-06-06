279c0033964d0b288b3eed9af5c690776c0f3bb6
ingfosbreak <ingfosbreak@outlook.com>
2023-06-06 19:18:15 +0700

    Finished: mainpage other external

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function chbg1(color) {
-    document.body.style.background = color;
+    document.getElementById('c-odin').style.backgroundColor = color;
     document.getElementById('speech1').style.color = 'white'
     document.getElementById('speech2').style.color = 'white'
     
 }  
 
 function chbg2(color) {
-    document.body.style.background = color;
+    document.getElementById('c-odin').style.backgroundColor = color;
     document.getElementById('speech1').style.color = 'black'
     document.getElementById('speech2').style.color = 'black'
     
